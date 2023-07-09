c73e5e07a1bf80fd73084d2628e2db1cd67c0692
avishgah <138613565+avishgah@users.noreply.github.com>
2023-07-09 18:19:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,41 @@ import { Fragment } from 'react';
 import Header from './header';
 import Routes from './routes';
 import Footer from './footer';
+import { Button, Stack } from '@mui/material';
+import Takes from './take/Takes';
+import AddUser from './AddUser/AddUser';
+import { Route, useNavigate } from 'react-router-dom';
+import Payment from './Payment';
+// import Picture from "./Picture"
 
 function App() {
+  const nav = useNavigate();
 
+  return (<>
 
-  return (
-    <><h1>eertetyt</h1>
+    <Routes>
+      <Route path="/Payment" element={<Payment />}></Route>
+      
+    </Routes>
+
+    <div className="App">
+      <h1>ברוכים הבאים</h1>
+      <Stack direction="row" spacing={2}>
+        {/* <Button color="secondary">Secondary</Button> */}
+        <Button variant="contained" color="success" onClick={() => nav('/Payment')} >
+          לקחת
+        </Button>
+
+        <Button variant="contained" color="success" onClick="">
+          להחזיר
+        </Button>
+      </Stack>
+
+      <Takes></Takes>
+      {/* <Picture /> */}
+      <AddUser></AddUser>
+    </div>
+
     <Fragment>
       <Header />
       <Routes />
